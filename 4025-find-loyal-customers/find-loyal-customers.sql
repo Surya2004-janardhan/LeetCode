cebaa3e24de0728fd5205d90586fdf7e171b3eb6
Surya Janardhan <121177774+Surya2004-janardhan@users.noreply.github.com>
2025-08-24 22:50:41 +0530

    Time: 330 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 4025-find-loyal-customers/find-loyal-customers.sql
+++ 4025-find-loyal-customers/find-loyal-customers.sql
@@ -7,6 +7,6 @@ having count( if(transaction_type = 'purchase', 1, null ) ) >2
 
 and datediff(max(transaction_date) , min(transaction_date)) > 29 
 
-and (count( if (transaction_type = 'refund', 1, null) ) / count(1)) < 0.2
+and (count( if (transaction_type = 'refund', 1, null) ) / count(*)) < 0.2
 
 order by customer_id ;
