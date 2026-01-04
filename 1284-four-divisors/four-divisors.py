e49de8f1413ce48ddb0c4fa6d89ec185f578177a
Surya Janardhan <121177774+Surya2004-janardhan@users.noreply.github.com>
2026-01-04 11:02:37 +0530

    Time: 151 ms (72.13%) | Memory: 18.8 MB (58.88%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.py
@@ -0,0 +1,28 @@
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        hasher = Counter(nums)
+        ans = 0
+
+        def is_4_divisors(num):
+            divisors = [1, num]
+            for i in range(2,int(num**0.5)+1):
+                if len(divisors) > 4:
+                    return False, 0
+                if num % i == 0:
+                    divisors.append(i)
+                    if i != num//i:
+                        divisors.append(num//i)
+            if len(divisors) == 4:
+                print(divisors, num)
+                return True, sum(divisors)
+            print(divisors, num)
+
+            return False, 0
+
+        for k in hasher:
+            valid_num , csum = is_4_divisors(k)
+
+            if valid_num:
+                # print(k, csum)
+                ans += csum*hasher[k]
+        return ans 
